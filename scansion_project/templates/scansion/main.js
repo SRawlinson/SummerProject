82391a67316e0bfcf1c37d93fb63ad0e0ad35ee0
Stuart Rawlinson <s.rawlinson89@gmail.com>
2019-06-28 14:59:11 +0100

    broke it

---
+++ scansion_project/templates/scansion/main.js
@@ -0,0 +1,5 @@
+// function textEntered() {
+//     alert("got here");
+//     var enterredText = document.getElementById("text-input").value;
+//     document.getElementById("scan-output").value = enterredText;
+// }
