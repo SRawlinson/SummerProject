d3f7d140767536c3c1225e7fe2506c02d3ccee66
Stuart Rawlinson <s.rawlinson89@gmail.com>
2019-06-30 12:04:04 +0100

    Cleaned up JS file and started adding in translating into scansion notation -- need to look up regex stuff

--- scansion_project/static/js/main.js
+++ scansion_project/static/js/main.js
@@ -1,8 +1,4 @@
-var APIKey = "kcje7882nvil0mgncl2kio5pudxvpsz3ym5ispkp42ig69yqw";
-var baseJSONURL = "https://api.wordnik.com/v4/word.json/";
-var relatedWords = "/relatedWords?useCanonical=false&limitPerRelationshipType=10&api_key=kcje7882nvil0mgncl2kio5pudxvpsz3ym5ispkp42ig69yqw";
-var randomWordURL = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=kcje7882nvil0mgncl2kio5pudxvpsz3ym5ispkp42ig69yqw";
-var hyphenationURL = "/hyphenation?useCanonical=false&limit=50&api_key=";
+//Methods for page/display, tabs etc. 
 
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
@@ -21,71 +17,77 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-function textEntered() {
-    var enterredText = document.getElementById("text-input").value;
-    document.getElementById("scan-output").value = enterredText;
-    // alert();
-}
+//Methods and data do with calling the API
+
+var APIKey = "kcje7882nvil0mgncl2kio5pudxvpsz3ym5ispkp42ig69yqw";
+var baseJSONURL = "https://api.wordnik.com/v4/word.json/";
+var relatedWords = "/relatedWords?useCanonical=false&limitPerRelationshipType=10&api_key=kcje7882nvil0mgncl2kio5pudxvpsz3ym5ispkp42ig69yqw";
+var randomWordURL = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=kcje7882nvil0mgncl2kio5pudxvpsz3ym5ispkp42ig69yqw";
+var hyphenationURL = "/hyphenation?useCanonical=false&limit=50&api_key=";
 
 function getSyllables() {
-    syllables(document.getElementById("text-input").value, function (returned) {
-        alert("callback went");
-        //insert for loop here 
+    var thisurl = baseJSONURL + document.getElementById("text-input").value + hyphenationURL + APIKey;
+
+    callAPI(thisurl, function (returned) {
         var s = "";
-        // alert(s);
-        // document.getElementById("scan-ouput").innerHTML = (s);
+        var vowelFound = false;
 
         for (var i = 0; i < returned.length; i++) {
-            s += "text: " + returned[i].text + " "
-            if (returned[i].type) {
-                s += " stress ";
-            }
+            for (var j = 0; j < returned[i].text.length; j++) {
+                s += "x";
+
+
+            } 
         }
-        // console.log(s);
+
+
+
+        s += "\n" 
+        for (var i = 0; i < returned.length; i++) {
+            s += returned[i].text;
+            // if (i === returned.length-1) {
+
+            // } else {
+            //     s += " | ";
+            // }
+        }
+
+
+
+
+        // for (var i = 0; i < returned.length; i++) {
+        //     s += "text: " + returned[i].text + " "
+        //     if (returned[i].type) {
+        //         s += " stress ";
+        //     }
+        // }
         document.getElementById("scan-output").innerHTML = (s);
     });
 }
 
-function syllables(theWord, callback) {
-    alert("syllables went");
-    var thisurl = baseJSONURL + theWord + hyphenationURL + APIKey;
-    var jxhr = $.ajax ({
-        url: thisurl,
-        dataType: "text" ,
-        timeout: 30000
-    })
-    .success (function (data, status) {
-        var array = JSON.parse(data);
-        alert("success went");
-        callback (array)
-    })
-    .error (function (status) {
-        alert("Error went");
 
-    });
-}
 
 function getRandom() {
-    random("rainbow", function (words) {
-    alert(words.word);
+    callAPI(randomWordURL, function (words) {
+    // alert(words.word);
     document.getElementById("scan-output").innerHTML = words.word;
     });
 }
 
-function random(theWord, callback) {
-    alert("random went");
+function callAPI(theURL, callback) {
+    // alert("callAPI went");
     var jxhr = $.ajax ({
-        url: randomWordURL,
+        url: theURL,
         dataType: "text" ,
         timeout: 30000
     })
     .success (function (data, status) {
         var array = JSON.parse (data);
-        alert(".success went");
+        // alert(".success went");
         callback (array)
     })
     .error (function (status) {
-        alert(".error went");
+        // alert(".error went");
         console.log ("random: url ==" + url + ", error == " + JSON.stringify (status, undefined, 4));
     });
 
